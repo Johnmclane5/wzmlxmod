27e93f50dc3208a25533806b8429f8b3642f775d
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2024-01-02 18:32:48 +0530

    Update mirror_leech.py

--- bot/modules/mirror_leech.py
+++ bot/modules/mirror_leech.py
@@ -28,7 +28,6 @@ from bot.helper.listeners.tasks_listener import MirrorLeechListener
 from bot.helper.ext_utils.help_messages import MIRROR_HELP_MESSAGE
 from bot.helper.ext_utils.bulk_links import extract_bulk_links
 from bot.helper.mirror_utils.download_utils.direct_downloader import add_direct_download
-from bot.helper.ext_utils.aeon_utils import nsfw_precheck
 
 @new_task
 async def _mirror_leech(client, message, isQbit=False, isLeech=False, sameDir=None, bulk=[]):
@@ -213,8 +212,6 @@ async def _mirror_leech(client, message, isQbit=False, isLeech=False, sameDir=No
 
     error_msg = []
     error_button = None
-    if await nsfw_precheck(message):
-    	  error_msg.extend(['NSFW detected'])
     task_utilis_msg, error_button = await task_utils(message)
     if task_utilis_msg:
         error_msg.extend(task_utilis_msg)
