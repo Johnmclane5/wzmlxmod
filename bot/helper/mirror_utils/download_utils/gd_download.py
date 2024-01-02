9728b3dc550e2feb7a847ca0c9d06c9c870b957d
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2024-01-02 18:20:14 +0530

    Update gd_download.py

--- bot/helper/mirror_utils/download_utils/gd_download.py
+++ bot/helper/mirror_utils/download_utils/gd_download.py
@@ -7,8 +7,6 @@ from bot.helper.mirror_utils.status_utils.queue_status import QueueStatus
 from bot.helper.telegram_helper.message_utils import sendMessage, sendStatusMessage, five_minute_del
 from bot.helper.ext_utils.bot_utils import sync_to_async
 from bot.helper.ext_utils.task_manager import is_queued, limit_checker, stop_duplicate_check
-from bot.helper.ext_utils.aeon_utils import isNSFW, isNSFWdata
-
 
 async def add_gd_download(link, path, listener, newname):
     drive = GoogleDriveHelper()
@@ -20,11 +18,7 @@ async def add_gd_download(link, path, listener, newname):
     data = drive.getFilesByFolderId(id)
     name = newname or name
     gid = token_hex(4)
-    
-    if isNSFW(name) or isNSFWdata(data):
-        await listener.onDownloadError('NSFW detected')
-        return
-    
+
     msg, button = await stop_duplicate_check(name, listener)
     if msg:
         await sendMessage(listener.message, msg, button)
